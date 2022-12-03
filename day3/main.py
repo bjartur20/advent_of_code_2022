b8bac193a06eab0cf87094db81fdc65b54192ace
bjartur20 <bjartur20@gmail.com>
2022-12-03 16:17:41 +0000

    day3 part2 complete

--- day3/main.py
+++ day3/main.py
@@ -1,7 +1,12 @@
-def part1():
+def get_priority(item):
     lowercase_offset = 96
     uppercase_offset = 64
+    if item.islower():
+        return ord(item) - lowercase_offset
+    
+    return ord(item) - uppercase_offset + 26
 
+def part1():
     with open('day3/input.txt') as f:
         priority = 0
         for bag_raw in f.readlines():
@@ -10,23 +15,34 @@ def part1():
 
             for item in bag[:nr_of_items//2]:
                 if item in bag[nr_of_items//2:]:
-                    if item.islower():
-                        priority += ord(item) - lowercase_offset
-                    else:
-                        priority += ord(item) - uppercase_offset + 26
+                    priority += get_priority(item)
                     break
 
     return priority
 
 
 def part2():
-    ...
+    lowercase_offset = 96
+    uppercase_offset = 64
+
+    with open('day3/input.txt') as f:
+        bags = f.readlines()
+        priority = 0
+        for idx in range(0, len(bags), 3):
+            one, two, three = bags[idx].strip(), bags[idx + 1].strip(), bags[idx + 2].strip()
+
+            for item in one:
+                if item in two and item in three:
+                    priority += get_priority(item)
+                    break
+
+    return priority
 
 def main():
     print("Day 2")
 
     print(f"\tPart 1: {part1()}")
-    # print(f"\tPart 2: {part2()}")
+    print(f"\tPart 2: {part2()}")
 
 if __name__ == "__main__":
     main()
